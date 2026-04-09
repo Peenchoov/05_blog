21e69d558e8badfebfd65f283509152effcbb718
Peenchoov <shrgn00@gmail.com>
2026-04-09 22:22:21 +0200

    Added template for CreateView class to create new blogs

--- blog/views.py
+++ blog/views.py
@@ -15,5 +15,5 @@ class BlogDetailView(DetailView):
 
 class BlogCreateView(CreateView):
     model = Post
-    template_name = "home.html"
+    template_name = "post_new.html"
     fields = ["title", "author", "body"]
